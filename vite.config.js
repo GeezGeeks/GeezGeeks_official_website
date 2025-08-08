bdfff0ef220e8c39bf2abdf2e740963c37ebef74
Lidet <lidetadmassu217@gmail.com>
2025-08-08 23:55:17 +0300

    Bug Fix

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    headers: {
+      "X-Content-Type-Options": "nosniff",
+    },
+  },
 });
